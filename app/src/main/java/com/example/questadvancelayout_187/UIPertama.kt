2635109a301f5e40ede6748b0af55b65b7d216a9
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-10-24 17:40:49 +0700

    add Composable Function Image

--- app/src/main/java/com/example/questadvancelayout_187/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_187/UIPertama.kt
@@ -12,12 +12,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -35,6 +37,13 @@ fun ActivitasPertama(modifier: Modifier = Modifier){
             .padding(horizontal = 24.dp, vertical = 40.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+        )
         Text(
             stringResource(R.string.prodi),
             fontSize = 35.sp,
