66dadf05a7db7fb5ec42a32bd0f0e7fce05a8158
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-10-24 17:59:04 +0700

    delete unused code

--- app/src/main/java/com/example/questadvancelayout_187/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_187/UIPertama.kt
@@ -86,64 +86,6 @@ fun ActivitasPertama(modifier: Modifier = Modifier){
             fontSize = 12.sp,
             modifier = Modifier.padding(bottom = 12.dp)
         )
-
-        Text(
-            stringResource(R.string.prodi),
-            fontSize = 35.sp,
-            fontWeight = FontWeight.Bold,
-        )
-        Text(
-            stringResource(R.string.univ),
-            fontSize = 22.sp,
-            textAlign = androidx.compose.ui.text.style.TextAlign.Center
-        )
-        Spacer(modifier = Modifier.height(25.dp))
-        Card(
-            modifier = Modifier
-                .fillMaxWidth(1f)
-                .padding(12.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = Color.DarkGray
-            )
-        ) {
-            Row() {
-                val gambar = painterResource(R.drawable.logo_umy)
-                Image(
-                    painter = gambar,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(100.dp)
-                        .padding(5.dp)
-                )
-                Spacer(modifier = Modifier.width(30.dp))
-                Column() {
-                    Text(
-                        stringResource(R.string.nama),
-                        fontSize = 30.sp,
-                        fontFamily = FontFamily.Cursive,
-                        color = Color.White,
-                        modifier = Modifier.padding(top = 15.dp)
-                    )
-                    Text(
-                        stringResource(R.string.alamat),
-                        fontSize = 20.sp,
-                        color = Color.Yellow,
-                        modifier = Modifier.padding(top = 10.dp)
-                    )
-                }
-            }
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-        ){
-            Text(
-                text = stringResource(id = R.string.copy),
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 50.dp)
-            )
-        }
     }
 }
 
